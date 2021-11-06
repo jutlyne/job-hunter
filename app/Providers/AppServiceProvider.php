5d96563a0c4494054a2bbc90507acf785b041891
kyvc-flydino <kyvc@flydinotech.com>
2021-11-06 17:52:54 +0700

    deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,14 +33,14 @@ class AppServiceProvider extends ServiceProvider
         JsonResource::withoutWrapping();
 
         //share data blog categories
-        $shareBlogCategories = Category::all();
-        View::share('shareBlogCategories', $shareBlogCategories);
-        //share data 3 blogs
-        $shareDataBlog = Blog::orderBy('created_at', 'desc')->take(3)->get();
-        View::share('shareDataBlog', $shareDataBlog);
-        //share data profile
-        $shareDataProfile = Admin::take(1)->get();
-        View::share('shareDataProfile', $shareDataProfile);
+        // $shareBlogCategories = Category::all();
+        // View::share('shareBlogCategories', $shareBlogCategories);
+        // //share data 3 blogs
+        // $shareDataBlog = Blog::orderBy('created_at', 'desc')->take(3)->get();
+        // View::share('shareDataBlog', $shareDataBlog);
+        // //share data profile
+        // $shareDataProfile = Admin::take(1)->get();
+        // View::share('shareDataProfile', $shareDataProfile);
     }
 
 }
